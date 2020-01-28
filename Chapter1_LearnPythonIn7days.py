ab0e29f8921eec9b244213d49816d043615d4de0
JaysreeBora <60377460+JaysreeBora@users.noreply.github.com>
2020-01-28 17:34:26 +0530

    Uploading the chapter 1 programs

---
+++ Chapter1_LearnPythonIn7days.py
@@ -0,0 +1,84 @@
+#back_slash
+print "hello \
+world"
+
+#double quotes
+print "Hello World!"
+
+print "Hello World!"
+print "Hello World!"
+
+#This is a single line comment in Python
+print "hello world"
+"""For multiple comments
+use three qoutes"""
+print "Bye"
+
+print ''' I am mad in love
+	do you think
+		I am doing
+			the right thing'''
+
+print """ I am mad in love
+	do you think
+		I am doing		
+			the right thing"""
+			
+#This is a single line comment in Python
+
+print "Hello World" #This is a single comment in Python
+
+""" For multi-line 
+comment use three 
+double quotes
+...
+"""
+print "Hello World!"
+
+#single quote inside double
+print "Hey there it's a cow"
+			
+print "hey there it's a cow"
+print "Hello all 'Mr' Choudhury is here"
+
+#double quote inside single quote
+print 'old world "but" still good'
+
+#escape_sequence
+print '\a'
+print '\a'
+print '\a'
+print '\t\tDeep'
+print "i know, they are 'great'"
+print '\a'
+
+#formatted_output
+print "Name", "Marks", "Age" 
+print "John Doe", 80.67, "27" 
+print "Bhaskar", 76.908, "27"
+print "Mohit", 56.98, "25"
+
+print 
+
+print "Name Marks Age" 
+print "John Doe 80.67 27" 
+print "Bhaskar 76.908 27"
+print "Mohit 56.98 25"
+
+print
+
+print "Name Marks Age" 
+print ( "%s %14.2f %11d" % ("John Doe", 80.67, 27)) 
+print ( "%s %12.2f %11d" %("Bhaskar" ,76.901, 27))
+print ( "%s %3.2f %11d" %("Mohit", 56.98, 25))
+
+print
+
+print ("%s %14.2f %14d" %("Jaysree", 90.789, 28.5))
+
+#string_concatenation
+print "Only way to join" + " two strings"
+
+#strings_inside_quotes
+print "Hello World 'Mr' Bond" 
+print 'old world "but" still good'
